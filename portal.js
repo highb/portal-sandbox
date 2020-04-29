22dd0a8f24a8b4366e5e2b7e02422a89bd53ce9c
Brandon High <brandon.high@puppet.com>
2020-04-29 13:20:08 -0700

    Add portal depth customization

--- portal.js
+++ portal.js
@@ -42,26 +42,39 @@ style.innerHTML = `
     100% { opacity: 1; }
   }
 `;
-if (window.portalHost) {
-  // Customize the UI when being embedded as a portal
 
-} else {
-  const portal = document.createElement('portal');
-  // Begin portal recursion
-  portal.src = 'https://highb.github.io/portal-sandbox/';
-  // Add a class that defines the transition. Consider using
-  // `prefers-reduced-motion` media query to control the animation.
-  // https://developers.google.com/web/updates/2019/03/prefers-reduced-motion
-  portal.classList.add('portal-transition');
-  portal.addEventListener('click', evt => {
-    // Animate the portal once user interacts
-    portal.classList.add('portal-reveal');
-  });
-  portal.addEventListener('transitionend', evt => {
+const portal = document.createElement('portal');
+// Begin portal recursion
+portal.src = 'https://highb.github.io/portal-sandbox/';
+// Add a class that defines the transition. Consider using
+// `prefers-reduced-motion` media query to control the animation.
+// https://developers.google.com/web/updates/2019/03/prefers-reduced-motion
+portal.classList.add('portal-transition');
+portal.addEventListener('click', evt => {
+  // Animate the portal once user interacts
+  portal.classList.add('portal-reveal');
+});
+portal.addEventListener('transitionend', evt => {
+  if (window.portalHost) {
+    // Receive message via window.portalHost
+    window.portalHost.addEventListener('portalactivate', evt => {
+      const portal_depth = evt.data.portal_depth;
+      // handle the event
+      // Customize the UI when being embedded as a portal
+      text = document.createElement('p');
+      text.innerHTML = `Remaining portals` + portal_depth
+      document.body.append(text, portal);
+      if portal_depth > 0 {
+        if (evt.propertyName == 'transform') {
+          portal.activate({ data: { 'portal_depth': portal_depth } });
+        }
+      }
+    });
+  } else {
     if (evt.propertyName == 'transform') {
       // Activate the portal once the transition has completed
-      portal.activate();
+      portal.activate({data: {'portal_depth': 2}});
     }
-  });
-  document.body.append(style, portal);
-}
+  }
+});
+document.body.append(style, portal);
